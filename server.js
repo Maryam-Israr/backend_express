b502b6912b64fc645b6d53a6cbed828c84799fe0
Maryam-Israr <23pwbcs0993@uetpeshawar.edu.pk>
2025-01-11 11:45:39 +0500

    Update server.js

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ app.use('/api/quizzes', quizRoutes);
 app.use('/api/users',userRouter);
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 60000, // 60 seconds
+}).then(() => { console.log('connected') }).catch(error=>console.log(error));
 
 app.listen(5000, () => console.log('Server running on port 5000'));
